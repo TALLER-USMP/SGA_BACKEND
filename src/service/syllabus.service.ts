5b9df414b7355b942f3dcd8d5ffd2f8458f487b5
Ghostcito <jmguticasani1254@gmail.com>
2025-10-04 00:45:56 -0500

    feat: endpoint update competencies components

--- src/service/syllabus.service.ts
+++ src/service/syllabus.service.ts
@@ -6,9 +6,17 @@ export class SyllabusService {
   async getOne(id: string) {
     const syllabus = await this.syllabusRepo.findById(id);
     if (!syllabus) {
-      throw new Error("Usuario no encontrado");
+      throw new Error("Silabo no encontrado");
     }
 
     return syllabus;
+  }
+
+  async updateCompetencias(id: number, data: any[]) {
+    if (!data || data.length === 0) {
+      throw new Error("No se proporcionaron competencias para actualizar");
+    }
+
+    return await this.syllabusRepo.updateCompetencies(id, data);
   }
 }
